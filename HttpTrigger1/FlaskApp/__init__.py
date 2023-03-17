e1e06ce0f9650dcde4cbf68714802c61d138f22d
hyc3z <tubao9hao@126.com>
2023-03-18 00:41:50 +0800

    Commi

--- HttpTrigger1/FlaskApp/__init__.py
+++ HttpTrigger1/FlaskApp/__init__.py
@@ -1,30 +1,42 @@
 from revChatGPT.V1 import Chatbot
 import os
-session_token = os.environ.get('SESSION_TOKEN')
-chatbot = Chatbot(config = {
-    "session_token": session_token
-})
+import openai
+openai.api_type = "azure"
+openai.api_base = "https://bob.openai.azure.com/"
+openai.api_version = "2022-12-01"
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
 from flask import Flask, request
+
+system_message_template = "<|im_start|>system\n{}\n<|im_end|>"
+system_message = system_message_template.format("")
+
+def create_prompt(system_message, messages):
+    prompt = system_message
+    message_template = "\n<|im_start|>{}\n{}\n<|im_end|>"
+    for message in messages:
+        prompt += message_template.format(message['sender'], message['text'])
+    prompt += "\n<|im_start|>assistant\n"
+    return prompt
+
+messages = [{"sender": "user", "text": "USER_INPUT_GOES_HERE"}]
 
 app = Flask(__name__)
 @app.route('/chat', methods = ['POST'])
-def hello_world():
+def track():
     return_message = ""
     if request.method == 'POST':
-        """modify/update the information for <user_id>"""
-        # you can use <user_id>, which is a str but could
-        # changed to be int or whatever you want, along
-        # with your lxml knowledge to make the required
-        # changes
         input_data = request.get_data().decode('utf-8')
-        prev_text = ""
-        for data in chatbot.ask(
-            input_data,
-        ):
-            message = data["message"][len(prev_text) :]
-            return_message += message
-            prev_text = data["message"]
-        return return_message
+        response = openai.Completion.create(
+            engine="Agda",
+            prompt= create_prompt(system_message_template.format(input_data), messages),
+            temperature=0.19,
+            max_tokens=800,
+            top_p=0.06,
+            frequency_penalty=0,
+            presence_penalty=0,
+            stop=["<|im_end|>"])
+        return response
 
 if __name__ == "__main__":
     app.run()
