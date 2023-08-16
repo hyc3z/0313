7e45eead5ffbfeb2556b175fa0575464134e143e
hyc3z <tubao9hao@126.com>
2023-08-16 23:33:11 +0800

    add

--- gpt_flutter_redux/lib/services/assets_manager.dart
+++ gpt_flutter_redux/lib/services/assets_manager.dart
@@ -1,6 +1,6 @@
 class AssetsManager {
   static String imagePath = "assets/images";
   static String userImage = "$imagePath/hulu_logo.jpeg";
-  static String botImage = "$imagePath/openai_logo.jpeg";
-  static String openaiLogo = "$imagePath/openai_logo.jpeg";
+  static String botImage = "$imagePath/openai_logo.jpg";
+  static String openaiLogo = "$imagePath/openai_logo.jpg";
 }
